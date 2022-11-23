ecc9222b99779a041bc81d65dbcb77a4080e27c5
harveyyue <yw_yuewei@126.com>
2022-11-23 12:07:15 +0800

    [Improve][Connector-V2][Hive] Avoid to provide hive metastore uri parameter

--- seatunnel-connectors-v2/connector-hive/src/main/java/org/apache/seatunnel/connectors/seatunnel/hive/source/HiveSource.java
+++ seatunnel-connectors-v2/connector-hive/src/main/java/org/apache/seatunnel/connectors/seatunnel/hive/source/HiveSource.java
@@ -23,11 +23,9 @@ import static org.apache.seatunnel.connectors.seatunnel.hive.config.HiveConfig.T
 import static org.apache.hadoop.fs.CommonConfigurationKeysPublic.FS_DEFAULT_NAME_KEY;
 
 import org.apache.seatunnel.api.common.PrepareFailException;
-import org.apache.seatunnel.api.common.SeaTunnelAPIErrorCode;
 import org.apache.seatunnel.api.source.SeaTunnelSource;
 import org.apache.seatunnel.common.config.CheckConfigUtil;
 import org.apache.seatunnel.common.config.CheckResult;
-import org.apache.seatunnel.common.constants.PluginType;
 import org.apache.seatunnel.common.exception.CommonErrorCode;
 import org.apache.seatunnel.connectors.seatunnel.file.config.BaseSourceConfig;
 import org.apache.seatunnel.connectors.seatunnel.file.config.FileFormat;
@@ -59,38 +57,35 @@ public class HiveSource extends BaseHdfsFileSource {
     public void prepare(Config pluginConfig) throws PrepareFailException {
         CheckResult result = CheckConfigUtil.checkAllExists(pluginConfig, HiveConfig.METASTORE_URI.key(),
                 HiveConfig.TABLE_NAME.key());
-        if (!result.isSuccess()) {
-            throw new HiveConnectorException(SeaTunnelAPIErrorCode.CONFIG_VALIDATION_FAILED,
-                    String.format("PluginName: %s, PluginType: %s, Message: %s",
-                            getPluginName(), PluginType.SOURCE, result.getMsg()));
-        }
-        Pair<String[], Table> tableInfo = HiveConfig.getTableInfo(pluginConfig);
-        tableInformation = tableInfo.getRight();
-        String inputFormat = tableInformation.getSd().getInputFormat();
-        if (TEXT_INPUT_FORMAT_CLASSNAME.equals(inputFormat)) {
-            pluginConfig = pluginConfig.withValue(BaseSourceConfig.FILE_TYPE.key(),
-                    ConfigValueFactory.fromAnyRef(FileFormat.TEXT.toString()));
-        } else if (PARQUET_INPUT_FORMAT_CLASSNAME.equals(inputFormat)) {
-            pluginConfig = pluginConfig.withValue(BaseSourceConfig.FILE_TYPE.key(),
-                    ConfigValueFactory.fromAnyRef(FileFormat.PARQUET.toString()));
-        } else if (ORC_INPUT_FORMAT_CLASSNAME.equals(inputFormat)) {
-            pluginConfig = pluginConfig.withValue(BaseSourceConfig.FILE_TYPE.key(),
-                    ConfigValueFactory.fromAnyRef(FileFormat.ORC.toString()));
-        } else {
-            throw new HiveConnectorException(CommonErrorCode.ILLEGAL_ARGUMENT,
-                    "Hive connector only support [text parquet orc] table now");
-        }
-        String hdfsLocation = tableInformation.getSd().getLocation();
-        try {
-            URI uri = new URI(hdfsLocation);
-            String path = uri.getPath();
-            String defaultFs = hdfsLocation.replace(path, "");
-            pluginConfig = pluginConfig.withValue(BaseSourceConfig.FILE_PATH.key(), ConfigValueFactory.fromAnyRef(path))
-                .withValue(FS_DEFAULT_NAME_KEY, ConfigValueFactory.fromAnyRef(defaultFs));
-        } catch (URISyntaxException e) {
-            String errorMsg = String.format("Get hdfs namenode host from table location [%s] failed," +
-                    "please check it", hdfsLocation);
-            throw new HiveConnectorException(HiveConnectorErrorCode.GET_HDFS_NAMENODE_HOST_FAILED, errorMsg, e);
+        if (result.isSuccess()) {
+            Pair<String[], Table> tableInfo = HiveConfig.getTableInfo(pluginConfig);
+            tableInformation = tableInfo.getRight();
+            String inputFormat = tableInformation.getSd().getInputFormat();
+            if (TEXT_INPUT_FORMAT_CLASSNAME.equals(inputFormat)) {
+                pluginConfig = pluginConfig.withValue(BaseSourceConfig.FILE_TYPE.key(),
+                        ConfigValueFactory.fromAnyRef(FileFormat.TEXT.toString()));
+            } else if (PARQUET_INPUT_FORMAT_CLASSNAME.equals(inputFormat)) {
+                pluginConfig = pluginConfig.withValue(BaseSourceConfig.FILE_TYPE.key(),
+                        ConfigValueFactory.fromAnyRef(FileFormat.PARQUET.toString()));
+            } else if (ORC_INPUT_FORMAT_CLASSNAME.equals(inputFormat)) {
+                pluginConfig = pluginConfig.withValue(BaseSourceConfig.FILE_TYPE.key(),
+                        ConfigValueFactory.fromAnyRef(FileFormat.ORC.toString()));
+            } else {
+                throw new HiveConnectorException(CommonErrorCode.ILLEGAL_ARGUMENT,
+                        "Hive connector only support [text parquet orc] table now");
+            }
+            String hdfsLocation = tableInformation.getSd().getLocation();
+            try {
+                URI uri = new URI(hdfsLocation);
+                String path = uri.getPath();
+                String defaultFs = hdfsLocation.replace(path, "");
+                pluginConfig = pluginConfig.withValue(BaseSourceConfig.FILE_PATH.key(), ConfigValueFactory.fromAnyRef(path))
+                        .withValue(FS_DEFAULT_NAME_KEY, ConfigValueFactory.fromAnyRef(defaultFs));
+            } catch (URISyntaxException e) {
+                String errorMsg = String.format("Get hdfs namenode host from table location [%s] failed," +
+                        "please check it", hdfsLocation);
+                throw new HiveConnectorException(HiveConnectorErrorCode.GET_HDFS_NAMENODE_HOST_FAILED, errorMsg, e);
+            }
         }
         super.prepare(pluginConfig);
     }
